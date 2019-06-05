45f60f8a7046ed9307de5e6342b929234391daf0
Simone Tolomei <tolomei.simone5@gmail.com>
2019-06-05 11:20:57 +0200

    Prima versione SW completo

--- Muletto_software/Muletto_software.ino
+++ Muletto_software/Muletto_software.ino
@@ -1,3 +1,5 @@
+#include <COLORPAL.h>
+#include <Servo.h>
 #include <QTRSensors.h>
 
 #define buzzer 7
@@ -11,8 +13,11 @@
 #define P_BLU A0
 
 #define K 0.05
+enum {rosso, verde, blu, nessuno} colore, coloreDest; //ci serve per scrivere nel programma i colori
 
 QTRSensors qtr;
+Servo carrello;
+Colorpal colorp(10); //Sul Pin 10, credo
 
 const uint8_t SensorCount = 3;
 uint16_t sensorValues[SensorCount];
@@ -20,29 +25,98 @@ int basevel = 80;
 uint16_t position;
 int error;
 int vel;
+int nStop;
+int c_rosso, c_verde, c_blu; // 3 variabili per le 3 componenti di colore
+
 void setup()
 {
   prepara_Sensori();
   //set motors
-  pinMode(DIRA, OUTPUT);  
+  pinMode(DIRA, OUTPUT);
   pinMode(DIRB, OUTPUT);
   digitalWrite(DIRA, LOW);  //imposto direzione
   digitalWrite(DIRB, HIGH);
-  
+  //preparo i pulsanti
+  pinMode(P_ROSSO, INPUT_PULLUP);
   pinMode(P_VERDE, INPUT_PULLUP);
-  
-  while (digitalRead(P_VERDE)); //aspetta finchè non premo il pulsante verde
-
-
+  pinMode(P_BLU, INPUT_PULLUP);
+  //preparo colorpal e servomotore
+  carrello.attach(9);
+  colorp.init();
+  coloreDest = nessuno;
 }
 
 void loop() {
-  //linefollow
-  position = qtr.readLineBlack(sensorValues);
-  error = 1000 - position;
-  vel = K * error;
-  analogWrite(PWMA, (basevel - vel));
-  analogWrite(PWMB, (basevel + vel));
+  //aspetto finché non viene premuto un pulsante di colore
+  while (coloreDest = nessuno) {
+    if (!digitalRead(P_ROSSO)) coloreDest = rosso;
+    if (!digitalRead(P_VERDE)) coloreDest = verde;
+    if (!digitalRead(P_BLU)) coloreDest = blu;
+  }
+  //Quando esco di qui coloreDest contiene il colore da trovare.
+  //creo un ciclo che ri ripete finché non trovo il colore giusto
+  colore = nessuno;
+  nStop = 0;
+  while (colore != coloreDest) {
+    linefollow(); //seguo finché non trovo una tacca di stop, poi leggo il colore
 
+    colorp.readRGB(c_rosso, c_verde, c_blu); //leggo le 3 componenti
+    int cmax = max(c_rosso, c_verde); //prendo il max tra le 3 componenti di colore
+    cmax = max(cmax, blu);
+    if (cmax != 0) {       //controllo a cosa è uguale e assegno
+      if (cmax == c_verde) colore = verde;
+      if (cmax == c_rosso) colore = rosso;
+      if (cmax == c_blu) colore = blu;
+    }
+    nStop++;
+  }//quando esco di qui sarò sullo stop corrispondente al colore che devo prendereù
+
+  //MANOVRA DI ROTAZIONE
+  digitalWrite(DIRA, HIGH); //DX HIGH = INDIETRO
+  digitalWrite(DIRB, HIGH); //SX avanti
+  analogWrite(PWMA, 50);
+  analogWrite(PWMB, 50);
+  delay(1000);  //Si può fare con un delay o con la linea
+  //while((position = qtr.readLineBlack(sensorValues)) != 1000);
+  frena();
+
+  digitalWrite(DIRA, LOW); //AVVICINAMENTO
+  digitalWrite(DIRB, HIGH);
+  analogWrite(PWMA, 50);
+  analogWrite(PWMB, 50);
+  delay(1000);
+  frena();
+
+  //sollevo
+  carrello.write(80);
+  delay(1000);
+  carrello.write(90);
+
+  //vado indietro
+  digitalWrite(DIRA, HIGH);
+  digitalWrite(DIRB, LOW);
+  analogWrite(PWMA, 50);
+  analogWrite(PWMB, 50);
+  delay(1000);
+  frena();
+
+  //GIRO DI ALTRI 90° per tornare sulla linea
+  digitalWrite(DIRA, HIGH);
+  digitalWrite(DIRB, HIGH);
+  analogWrite(PWMA, 50);
+  analogWrite(PWMB, 50);
+  //delay(1000);
+  while ((position = qtr.readLineBlack(sensorValues)) != 1000);
+  frena();
+
+  while (nStop) { //torno indietro oltrepassando un numero di linee uguale a quello che ho contato
+    linefollow();
+    nStop--;
+  }
+  frena();
+  //abbassa carrello
+  carrello.write(100);
+  delay(1000);
+  carrello.write(90);
 
 }
--- Muletto_software/funzioni.ino
+++ Muletto_software/funzioni.ino
@@ -1,13 +1,36 @@
-void prepara_Sensori(){
-   // configure the sensors
+void prepara_Sensori() {
+  // configure the sensors
   qtr.setTypeRC();
-  qtr.setSensorPins((const uint8_t[]) { 5, 4, 2}, SensorCount);
+  qtr.setSensorPins((const uint8_t[]) {
+    5, 4, 2
+  }, SensorCount);
   delay(500);
   pinMode(buzzer, OUTPUT);
   tone(buzzer, 440, 1000);
   //calibrate for 5 seconds
-  for (uint16_t i = 0; i < 200; i++){
+  for (uint16_t i = 0; i < 200; i++) {
     qtr.calibrate();
   }
   tone(buzzer, 440, 500);
+}
+
+void linefollow() {
+  //linefollow. Segui finchè non trova lo stop
+  digitalWrite(DIRA, LOW);
+  digitalWrite(DIRB, HIGH); 
+  const int sogliaStop = 750;
+  do {
+    position = qtr.readLineBlack(sensorValues);
+    error = 1000 - position;
+    vel = K * error;
+    analogWrite(PWMA, (basevel - vel));
+    analogWrite(PWMB, (basevel + vel));
+  } while ((sensorValues[0] > sogliaStop) && (sensorValues[1] > sogliaStop) && (sensorValues[2] > sogliaStop));
+  analogWrite(PWMA, 0); //mi fermo
+  analogWrite(PWMB, 0);
+}
+
+void frena(){
+  analogWrite(PWMA,0);
+  analogWrite(PWMB,0);
 }
